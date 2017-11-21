3f60b1329d89984e98031c4d0124095d05902670
carloshj21 <carlos21p6-0800@comunidad.unam.mx>
2017-11-20 19:12:30 -0600

    Problema de suma de arreglos

--- Arreglos/Arreglo.java
+++ Arreglos/Arreglo.java
@@ -1,4 +1,58 @@
-class SumaArreglo
+class Arreglo
 {
+	private int[] sum = new int[5];
+	private int[] sum2 = new int[5];
+	private int [] res = new int[5];
 
+	public Arreglo()
+	{
+		System.out.println("\nBienvenido al sistema.\n");
+		/*sum[0]= 23;
+		System.out.println(cant1+"\n"+sum[0]);*/
+	}
+	public void asignaArre1 (int a1, int a2, int a3, int a4, int a5)
+	{
+		sum[0] = a1;
+		sum[1] = a2;
+		sum[2] = a3;
+		sum[3] = a4;
+		sum[4] = a5;
+
+		/*System.out.println(sum[0]);
+		System.out.println(sum[1]);
+		System.out.println(sum[2]);
+		System.out.println(sum[3]);
+		System.out.println(sum[4]);*/
+	}
+
+	public void asignaArre2 (int b1, int b2, int b3, int b4, int b5)
+	{
+		sum2[0] = b1;
+		sum2[1] = b2;
+		sum2[2] = b3;
+		sum2[3] = b4;
+		sum2[4] = b5;
+
+		/*System.out.println(sum2[0]);
+		System.out.println(sum2[1]);
+		System.out.println(sum2[2]);
+		System.out.println(sum2[3]);
+		System.out.println(sum2[4]);*/
+	}
+
+	public void suma ()
+	{
+		res[0] = sum[0] + sum2[0];
+		res[1] = sum[1] + sum2[1];
+		res[2] = sum[2] + sum2[2];
+		res[3] = sum[3] + sum2[3];
+		res[4] = sum[4] + sum2[4];
+
+		System.out.println("\n\nLa sumas de cada una de las casillas del Arreglo 1 con su correspondiente en el Arreglo 2 son:\n");
+		System.out.println("Arreglo1[0] + Arreglo2[0] = " + res[0]);
+		System.out.println("Arreglo1[1] + Arreglo2[1] = " + res[1]);
+		System.out.println("Arreglo1[2] + Arreglo2[2] = " + res[2]);
+		System.out.println("Arreglo1[3] + Arreglo2[3] = " + res[3]);
+		System.out.println("Arreglo1[4] + Arreglo2[4] = " + res[4]);
+	}
 }
--- Arreglos/SumaArreglos.java
+++ Arreglos/SumaArreglos.java
@@ -1,9 +1,59 @@
 import java.io.*;
 class SumaArreglos
 {
-  public static void main (String [] args)
-  {
-    InputStreamReader isr = new InputStreamReader(System.in);
+	public static void main (String [] args)
+  	{
+  		int a1, a2, a3, a4, a5, b1, b2, b3, b4, b5; 
+  		String  a1Cad, a2Cad, a3Cad, a4Cad, a5Cad, b1Cad, b2Cad, b3Cad, b4Cad, b5Cad; 
+
+ 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader flujoE = new BufferedReader(isr);
-  }
+
+		try
+		{
+			Arreglo sum1 = new Arreglo();
+			System.out.print("Valor a registrar en la primer casilla del Arreglo 1: ");
+			a1Cad = flujoE.readLine();
+			a1 = Integer.parseInt(a1Cad);
+			System.out.print("Valor a registrar en la segunda casilla del Arreglo 1: ");
+			a2Cad = flujoE.readLine();
+			a2 = Integer.parseInt(a2Cad);
+			System.out.print("Valor a registrar en la tercer casilla del Arreglo 1: ");
+			a3Cad = flujoE.readLine();
+			a3 = Integer.parseInt(a3Cad);
+			System.out.print("Valor a registrar en la cuarta casilla del Arreglo 1: ");
+			a4Cad = flujoE.readLine();
+			a4 = Integer.parseInt(a4Cad);
+			System.out.print("Valor a registrar en la quinta casilla del Arreglo 1: ");
+			a5Cad = flujoE.readLine();
+			a5 = Integer.parseInt(a5Cad);
+
+			sum1.asignaArre1(a1,a2,a3,a4,a5);
+
+			System.out.print("\nValor a registrar en la primer casilla del Arreglo 2: ");
+			b1Cad = flujoE.readLine();
+			b1 = Integer.parseInt(b1Cad);
+			System.out.print("Valor a registrar en la segunda casilla del Arreglo 2: ");
+			b2Cad = flujoE.readLine();
+			b2 = Integer.parseInt(b2Cad);
+			System.out.print("Valor a registrar en la tercer casilla del Arreglo 2: ");
+			b3Cad = flujoE.readLine();
+			b3 = Integer.parseInt(b3Cad);
+			System.out.print("Valor a registrar en la cuarta casilla del Arreglo 2: ");
+			b4Cad = flujoE.readLine();
+			b4 = Integer.parseInt(b4Cad);
+			System.out.print("Valor a registrar en la quinta casilla del Arreglo 2: ");
+			b5Cad = flujoE.readLine();
+			b5 = Integer.parseInt(b5Cad);
+
+			sum1.asignaArre2(b1,b2,b3,b4,b5);
+
+
+			sum1.suma();
+		}
+		catch (Exception e)
+		{
+			System.err.println("Error: "+ e.getMessage());
+		}
+  	}
 }
