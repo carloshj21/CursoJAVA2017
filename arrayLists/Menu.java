3c91f3a2e98de7275a4b9ee7b3d4a5279d42726d
carloshj21 <carlos21p6-0800@comunidad.unam.mx>
2017-12-23 14:53:28 -0600

    Programa arrayLists

--- arrayLists/Menu.java
+++ arrayLists/Menu.java
@@ -2,16 +2,40 @@ import java.io.*;
 import java.util.*;
 class Menu
 {
-  private int num;
+  private int num,i;
   ArrayList <Float> numeros = new ArrayList <Float>();
 
-
-
-  public void altaNum ()
+	public void altaNum (Float numAdd)
   {
-    numeros.add(14.34F);
-    num = numeros.size();
-    System.out.println(num);
+    numeros.add(numAdd);
+    System.out.println("Numero registrado satisfactoriamente.");
   }
 
+  public void bajaNum (int inte)
+  {
+    numeros.remove(inte);
+    System.out.println("Numero eliminado satisfactoriamente.");
+  }
+
+  public void alterNum (int inte, Float numAlter)
+  {
+    numeros.set(inte, numAlter);
+    System.out.println("Numero cambiado satisfactoriamente.");
+  }
+
+  public void verNums ()
+  {
+    num = numeros.size();
+    System.out.println("\n\nNumero de elementos registrados: "+num+".");
+    if(num == 0)
+    {
+      System.out.println("No hay elementos registrados.");
+    }
+    else
+    {
+      for (i=0;i<num;i++) {
+    		System.out.println("\n["+i+"] "+numeros.get(i));
+    	}
+    }
+  }
 }
--- arrayLists/pruebaArrayLists.java
+++ arrayLists/pruebaArrayLists.java
@@ -1,25 +1,53 @@
 class arrayMenu extends Lectura
 {
-	public static void main (String [] args)
+	public static void main(String[] args)
 	{
 
-    char eleccion1;
+    char eleccion1,eleccion2;
+		int posi;
 
     Lectura lec1 = new Lectura();
-		Menu al1 = new Menu();
-    System.out.println("Hola!\nDesea ingresar al menu principal? (s/n)");
-    lec1.lecChar();
-    //System.out.println(chars);
-    eleccion1 = chars;
-    //System.out.println(eleccion1);
-    if(eleccion1 == 's')
-    {
-      System.out.println("\nEija una opcion:\n1) Dar de alta un numero.\n2) Dar de baja un numero.\n3)Cambiar un numero.");
-    }
-    else
-    {
-        System.out.println("\nHasta Pronto!");
-    }
-    //al1.altaNum();
+    Lectura lec2 = new Lectura();
+		Lectura lec3 = new Lectura();
+    Menu al1 = new Menu();
+
+		do
+		{
+			System.out.println("\nElija una opcion:\n1) Dar de alta un numero.\n2) Dar de baja un numero.\n3) Cambiar un numero.\n4) Ver los elementos registrados.");
+			lec2.lecChar();
+			eleccion2 = chars;
+			if (eleccion2 == '1')
+			{
+				System.out.println("\nIngrese el numero a registrar:");
+				lec3.lecFloat();
+				al1.altaNum(floati);
+			}
+			if (eleccion2 == '2')
+			{
+				al1.verNums();
+				System.out.println("\nIngrese el numero de la posicion del elemento que quiera dar de baja:");
+				lec3.lecInt();
+				al1.bajaNum(inte);
+			}
+			if (eleccion2 == '3')
+			{
+				al1.verNums();
+				System.out.println("\nIngrese el numero de la posicion del elemento que quiera cambiar:");
+				lec3.lecInt();
+				posi = inte;
+				System.out.println("\nIngrese el valor del numero nuevo:");
+				lec3.lecFloat();
+				al1.alterNum(posi,floati);
+			}
+			if(eleccion2 == '4')
+			{
+				al1.verNums();
+			}
+			System.out.println("\nDesea realizar otra accion? (s/n)");
+	    lec1.lecChar();
+	    eleccion1 = chars;
+		}
+		while(eleccion1 == 's');
+		System.out.println("\nHasta Pronto!");
 	}
 }
